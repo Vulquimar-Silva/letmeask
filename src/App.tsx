42a10e9ea6f6ee5aa20f12c84aa536033f941c85
Vulquimar-Silva <vuquimar@hotmail.com>
2021-06-22 03:40:29 -0300

    subindo segundo dia nwl

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from "./pages/NewRoom";
+
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <div>
-      <h1>Fala Dev, novo app vem aí.</h1>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
